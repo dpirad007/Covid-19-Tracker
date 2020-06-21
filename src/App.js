80f335ab38ebb97fbd39d8ded75ec06f21542085
Dion Pinto <dionpinto000@gmail.com>
2020-06-21 20:30:48 +0530

    fixed error in route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { Component }  from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import {  Navbar } from './components';
+import { Navbar } from "./components";
 
-
-import  maha  from './pages/maha';
-import home from './pages/home';
+import maha from "./pages/maha";
+import home from "./pages/home";
 
 class App extends Component {
-
-    render() {
-
-
-        return (
-            <div>
-            <Router>
-            <Navbar />
-                <Switch>
-                    <Route path="/global" component={home} />
-                    <Route path="/maharastra" component={maha}/>
-                </Switch>
-            </Router>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/maharastra" component={maha} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 
 const Navbar = () => {
-    return (
-        <AppBar position="static">
-        <Toolbar>
-            <Typography variant="h6">
-                COVID-Tracker
-            </Typography>
-            <Button color="inherit" component={Link} to="/global">Global</Button>
-            <Button color="inherit" component={Link} to="/maharastra">Maharastra</Button>
-        </Toolbar>
-        </AppBar>
-
-    )
-}
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">COVID-Tracker</Typography>
+        <Button color="inherit" component={Link} to="/">
+          Global
+        </Button>
+        <Button color="inherit" component={Link} to="/maharastra">
+          Maharastra
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
